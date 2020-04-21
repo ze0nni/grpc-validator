5892f1fde86a3b078084334a7bc4544e761a358f
e.blagodarev <ze0nni@gmail.com>
2020-04-21 18:18:00 +0300

    Add code for start gRPC server

--- grpc-validator.go
+++ grpc-validator.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/ze0nni/grpc-validator/api"
+
+	"google.golang.org/grpc"
+
+	"github.com/ze0nni/grpc-validator/server"
+)
+
 //go:generate protoc -I api\ api\proto\api.proto --go_out=plugins=grpc:api
 
-func main() {
+const port = ":50051"
 
+func main() {
+	listener, err := net.Listen("tcp", port)
+	if nil != err {
+		log.Fatal(err)
+	}
+	grpsServer := grpc.NewServer()
+	api.RegisterValidatorServer(grpsServer, server.NewValidatorServer())
+
+	if err := grpsServer.Serve(listener); nil != err {
+		log.Fatal(err)
+	}
 }
